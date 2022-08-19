e68eb60d63bc14006dd4f2467f0170eaed2955fb
takahiro0626 <takahiro0626fukushima@gmail.com>
2022-08-19 19:12:00 +0900

    setup gin

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	router := gin.Default()
+
+	router.GET("/books/:id", func(context *gin.Context) {
+		id := context.Param("id")
+		context.JSON(http.StatusOK, gin.H{
+			"status": gin.H{
+				"code":   http.StatusOK,
+				"status": "success",
+			},
+			"id": id,
+		})
+	})
+
+	return router
+}
+
+func main() {
+	router := setupRouter()
+	router.Run(":8080")
+}
